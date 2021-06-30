4037edd6c8e2888bc0653eb6502bf12feec99b39
hasanfuad <hasanmdfuad@hotmail.com>
2021-06-30 22:30:26 +0600

    inheritance commit

---
+++ inheritance.js
@@ -0,0 +1,22 @@
+
+class Parent{
+    constructor(){
+        this.fatherName = 'Ayub';
+    }
+}
+
+class Child extends Parent{
+    constructor(name){
+        super();
+        this.name = name;
+    }
+    getName(){
+        return this.name + " " + this.fatherName;
+    }
+}
+
+const child1 = new Child('Fahad');
+const child2 = new Child('Fuad');
+
+console.log(child1.getName());
+console.log(child2.getName());
