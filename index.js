447dbfc2f0e0bfd87c2af763d74776100e1927cc
hasanfuad <hasanmdfuad@hotmail.com>
2021-06-30 21:44:25 +0600

    spread operator commit

--- index.js
+++ index.js
@@ -32,4 +32,24 @@ const doubleIt = function myFunc(num){
 
 const firstArrow = num => num * 2;
 
-console.log(firstArrow(9));
+// console.log(firstArrow(9));
+
+// Spread operator
+const arr1 = [22,98,11,23,25];
+const arr2 = [11,22,19,29,34];
+
+// const newArr = arr1.concat(arr2)
+
+const newArr = [...arr1,6, ...arr2]
+
+// console.log(newArr);
+
+const num1 = 500;
+const num2 = 923;
+const num3 = 100;
+
+const totalNum = [num1, num2, num3];
+
+const allNum = Math.max(...totalNum)
+
+console.log(allNum);
